05eb020814511349698f78af08170ae84f835827
maduiep <emmanuelmaduip@gmail.com>
2021-03-18 14:56:44 +0100

    adding a click events to the button components

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types';
 
-const Button = ({color, text}) => {
- return <button style={{backgroundColor: color}} className="btn">{text}</button>
+const Button = ({color, text, onClick}) => {
+
+ return <button 
+ onClick={onClick}
+ style={{backgroundColor: color}} 
+ className="btn">{text}
+ </button>
  
 }
 
