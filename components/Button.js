d6451d563a4ce4b24d490f2f357e7ced21aa8ee0
maduiep <emmanuelmaduip@gmail.com>
2021-03-18 14:46:48 +0100

    adding dynamic contents to button element

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,18 @@
-const Button = () => {
- return <button className="btn">Add</button>
+import PropTypes from 'prop-types';
+
+const Button = ({color, text}) => {
+ return <button style={{backgroundColor: color}} className="btn">{text}</button>
  
+}
+
+Button.defaultProps = {
+ color: 'blue',
+ text: 'Add'
+}
+
+Button.propTypes = {
+ color: PropTypes.string,
+ text: PropTypes.string
 }
 
 export default Button
