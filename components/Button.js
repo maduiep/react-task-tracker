bd3aad1afd375af56602551eaf8226fcbf6ff1a7
maduiep <emmanuelmaduip@gmail.com>
2021-03-22 16:22:18 +0100

    added toggle functionality to the add button

--- components/Button.js
+++ components/Button.js
@@ -4,23 +4,19 @@ const Button = ({color, text, onClick}) => {
 
  return <button 
  onClick={onClick}
- style={btnColor} 
+ style={{backgroundColor: color}} 
  className="btn">{text}
  </button>
  
 }
 
 Button.defaultProps = {
- color: 'green',
+ color: 'steelblue',
 }
 
 Button.propTypes = {
  color: PropTypes.string,
  text: PropTypes.string
-}
-
-const btnColor = {
- backgroundColor: 'green'
 }
 
 export default Button
--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,24 @@
 import PropTypes from 'prop-types';
 import Button from './Button'
 
-const Header = ({title, menu}) => {
-  const onClick  = (e) => {
-  console.log(e)
- }
+const Header = ({title, onAdd, showAdd}) => {
+ 
 
  return (
   <header className="header"> 
     <h1>{title}</h1>
-    <Button color='green' text='Add' onClick={onClick}/>
+    <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
   </header>
  )
 }
 
 Header.defaultProps = {
  title: 'Task Tracker',
- menu: 'Machines'
 }
 
 Header.propTypes = {
  title: PropTypes.string.isRequired,
- menu: PropTypes.string
 }
+
 
 export default Header
